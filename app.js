dfe7092aba9d4638e96cab2d8fd8fbcabfe30041
KuaterCraft <kuatercraft@gmail.com>
2024-01-15 12:55:01 +0530

    Chart

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const moment = require("moment");
-
+const Chart = require("chartjs");
+const Utils = Chart.Utils;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
@@ -43,31 +44,57 @@ app.get('/', async (req, res) => {
         }
     });
     const response = await data.json();
-
     function formatDate(label) {
         const currentDate = new Date();
         const targetDate = new Date(currentDate.getTime() - label * 24 * 60 * 60 * 1000);
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return targetDate.toLocaleDateString('en-US', options);
     }
-
-    const isSmallScreen = true;;
+    const isSmallScreen = true;
 
     res.render('index', { data: response, user: req.user, css: res.cssUrl, formatDate: formatDate, isSmallScreen: isSmallScreen });
 });
 
-
-app.get("/data-9876543", async (req, res) => {
-    const data = await fetch("https://stats.uptimerobot.com/api/getMonitorList/4pl0qIcnIi?page=1&_=1705223288793", {
+app.post("/bootstrap-bundle-min-js", async (req, res) => {
+    const authHeader = req.headers.authorization;
+    const key = "a9-u1816316-2eac1ced9069c16f7ef10fde";
+    if (authHeader !== `Bearer ${key}`) {
+        return res.status(401).json({ error: "Unauthorised" })
+    }
+    const currentDate = Math.floor(Date.now() / 1000).toFixed(0);
+    const lastDate = currentDate - 7 * 60 * 60;
+    const data = await fetch(`https://int-api.uptimerobot.com/internal/monitors/795872626/response-times?start=${lastDate}&end=${currentDate}&timeFrame=CUSTOM`, {
         method: "GET",
         headers: {
             "Accept": "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + "a9-u1816316-2eac1ced9069c16f7ef10fde"
         }
     });
     const response = await data.json();
     res.json(response)
 });
+
+app.post("/bootstrap-bundle-min-js-2", async (req, res) => {
+    const authHeader = req.headers.authorization;
+    const key = "a9-u1816316-2eac1ced9069c16f7ef10fde";
+    if (authHeader !== `Bearer ${key}`) {
+        return res.status(401).json({ error: "Unauthorised" })
+    }
+    const currentDate = Math.floor(Date.now() / 1000).toFixed(0);
+    const lastDate = currentDate - 2 * 60 * 60;
+    const data = await fetch(`https://int-api.uptimerobot.com/internal/monitors/795872626/response-times?start=${lastDate}&end=${currentDate}&timeFrame=CUSTOM`, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + "a9-u1816316-2eac1ced9069c16f7ef10fde"
+        }
+    });
+    const response = await data.json();
+    res.json(response)
+});
+
 
 app.use((req, res, next) => {
     res.status(404).render('404', { user: req.user, css: res.cssUrl });
